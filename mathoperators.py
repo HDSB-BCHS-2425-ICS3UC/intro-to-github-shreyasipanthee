30a76c736929b6f643843c0df6a276f428f4903a
shreyasipanthee <shreyasipanthee@gmail.com>
2025-02-21 14:03:48 -0500

    Used different math functions in code

---
+++ mathoperators.py
@@ -0,0 +1,55 @@
+import math
+
+#Adds 2 numbers
+sum_add=3+250
+
+#Finds the difference between 3 and -250
+difference = 3-250
+
+#Finds the product of 3 2 and 5
+product = 3*2*5
+
+#finds the quotient
+quotient = 251/6
+
+#finds 2 to the power of 3
+exponent = 2**3
+
+#find the remainder when dividing 27 by 17
+remainder = 27%17
+
+#finds the squareroot of 9 using the math library
+squareroot = math.sqrt(9)
+
+#finds 2 to the power of 3 using the math library
+power= math.pow(2,3)
+
+#Prints Sum
+print(sum_add)
+
+#Prints Difference
+print(difference)
+
+#Prints Product
+print(product)
+
+#Prints Quotient
+print(quotient)
+
+#Prints Quotient as an integer data type
+print(int(quotient))
+
+#Prints Squareroot
+print(squareroot)
+
+#Prints Exponent
+print(exponent)
+
+#Prints Power
+print(power)
+
+#Prints Remainder
+print(remainder)
+
+#Prints Sum and Quotient as string data types
+print(str(sum_add) + " " + str(quotient))
