0e6a34f4ae7b6b556f28356048fc772ff02bcb4b
shreyasipanthee <shreyasipanthee@gmail.com>
2025-02-21 13:42:38 -0500

    Used Booleans and Data Types in Code

---
+++ pythonbasics.py
@@ -0,0 +1,45 @@
+#Author:Shreya
+#Date Modified:
+#Description:
+
+x = "2"
+y = 2
+
+#converting the variable x to an integer and adding it to y
+print (int(x) +y)
+
+#Converting variable y into a string and printing it
+print ("the number in variable y is "+str(y))
+
+#assigning true or false to the booleans
+true=1
+false=1
+bool(true) 
+bool(false)
+
+#printing the true and false booleans
+print(bool(true))
+print(bool(false))
+
+#Adding the true and false booleans
+print(bool(true)+bool(false))
+
+logged_in = True
+print(logged_in)
+
+logged_out = True
+print(logged_out)
+
+print(bool(logged_in + logged_out))
+
+"""
+#Creating a variable to store a name
+name = input("Enter you name: ")
+print("Your name is " + name)
+
+#Creating age variable to read the inputed age from the user
+age = int(input("enter your age"))
+"""
+
+grade = input("Enter your grade ")
+print("your grade is "+ grade)
